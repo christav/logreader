12cb3f78a0bc41ba2bcbad18ee7eaa3885d3eae9
Chris Tavares <cct@tavaresstudios.com>
2024-06-28 16:14:45 -0700

    More transform tests

--- test/stream-processing/chunks-to-lines-transform-test.ts
+++ test/stream-processing/chunks-to-lines-transform-test.ts
@@ -23,5 +23,30 @@ describe('line transform transform', () => {
     expect(lines[0]).to.eql('this is a line');
   });
 
+  it('should return lines in reverse order from chunk', async () => {
+    const chunks = [
+      Buffer.from('This is the old line\nThis is the new line\n')
+    ];
+    const lines: string[] = [];
 
+    await pipeline(
+      chunks,
+      makeToLinesTransform(),
+      async function *(source) {
+        (source as Readable).setEncoding('utf8');
+        for await (let line of source) {
+          lines.push(line);
+        }
+      }
+    );
+
+    expect(lines).to.have.lengthOf(2);
+    expect(lines[0]).to.eql('this is the new line');
+    expect(lines[1]).to.eql('This is the old line');
+  });
+
+  it('should return all lines split across chunks');
+  it('should not return partial line at start of first chunk');
+  it('should delimit lines by crlf');
+  it('should not output oldest line in chunk if invalid utf8');
 });
