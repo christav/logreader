37f75f143f0ab7e6079bbc661b2e2f879330bfda
Chris Tavares <cct@tavaresstudios.com>
2024-06-28 18:31:45 -0700

    Sketch out ToLinesTransform class
    Enough state here to make it work going to class
    for implementation. Keeping the constructor function
    as the public interface to keep the abstraction if
    useful in the future.

--- src/lib/log-streams/to-lines-transform.ts
+++ src/lib/log-streams/to-lines-transform.ts
@@ -1,4 +1,38 @@
-import { Transform } from 'stream';
+import { Transform, TransformCallback, TransformOptions } from 'stream';
+
+class ToLinesTransform extends Transform {
+  private pendingChunk: Buffer;
+  private firstChunk: boolean;
+
+  constructor(options: TransformOptions) {
+    super(options);
+
+    this.pendingChunk = Buffer.allocUnsafe(0);
+    this.firstChunk = true;
+  }
+
+  _transform(chunk: any, encoding: BufferEncoding, callback: TransformCallback): void {
+    // Prepend this chunk to pending chunk
+    // Isolate out first line from chunk
+    // Store first line as new pending chunk
+
+    // Convert rest of chunk of text
+    // split on line ends
+    // if firstChunk:
+    //    if chunk[-1] != \n:
+    //      skip last line (it's incomplete)
+    //    firstChunk = false
+    // for each line in chunk.reversed():
+    //   output(line)
+    // done
+  }
+
+  _flush(callback: TransformCallback): void {
+    // Convert pending chunk to text - will be one line
+    // output it
+    // done
+  }
+}
 
 export function makeToLinesTransform() {
   return new Transform({
