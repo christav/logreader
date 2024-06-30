cd195f23c8a4bd5df8a429e0a01fd49a02250d83
Chris Tavares <cct@tavaresstudios.com>
2024-06-29 23:27:05 -0700

    Playing with fastify
    Getting initial server set up to discover how
    to stream results. Also set up debugger for
    server.

--- index.ts
+++ index.ts
@@ -1,1 +1,32 @@
-console.log('Hello!');
+import { createReadStream } from 'fs';
+import { pipeline } from 'stream/promises';
+import Fastify from 'fastify';
+
+const server = Fastify({ logger: true });
+
+server.get('/logs', async function (req, reply) {
+  reply.type('application/json');
+  const sourceFile = `${__dirname}/../package.json`;
+  console.log(`Sending file ${sourceFile}`);
+  const source = createReadStream(sourceFile);
+  return reply.send(source);
+});
+
+async function run() {
+  await server.listen(
+    { port: 3001 },
+    function (err, address) {
+      if (err) {
+        console.log(`Startup failed, error ${err}`);
+      } else {
+        console.log(`Server started at ${address}`);
+      }
+    }
+  )
+}
+
+run()
+  .then(
+    () => console.log('Server exitting'),
+    (err) => console.log(`Server exitted with error ${err}`)
+  );
